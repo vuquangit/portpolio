a173a9b05f83ffe5f7b1d57314b42bb9dba497d5
vuquangit <vuquangit@gmail.com>
2019-04-28 15:25:30 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation'
+import Header from './components/Header'
 
 function App() {
     return <div>
       <Navigation />
+      <Header />
     </div>
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import headerimg from '../../img/header-bg.JPG'
+import './header.css'
+
+export default class Header extends React.Component{
+    render() {
+        return (
+            <div className="header-img">
+              
+            </div>
+        )
+    }
+}
