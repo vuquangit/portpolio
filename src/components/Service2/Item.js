0f9450ea8e057051c75df234c04bd4e4660360f7
vuquangit <vuquangit@gmail.com>
2019-05-05 14:22:02 +0700

    fix/serviceImg

--- src/components/Service2/Item.js
+++ src/components/Service2/Item.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Item = ({ logo, title, info }) => (
   <div className="col-md-4">
     <div className="service-img">
-      <img src={logo} alt="Service of CG" />
+      <img src={logo} alt="Service of CG" className="serviceimg" />
     </div>
     <h4 className="service-heading">{title}</h4>
     <p className="text-muted">{info}</p>
