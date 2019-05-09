0bfd9a70b7f257bf923f6e796f23431a9c61bb75
vuquangit <vuquangit@gmail.com>
2019-05-09 23:04:52 +0700

    add axios api

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import "./404.scss";
 
-export const Page404 = () => <h1 className="error404">This is error 404</h1>;
+export const Page404 = () => (
+  <h1 className="error404">Error 404: Page not found :(</h1>
+);
