7708e7e62d3ebdf598c77769b732081fcf134dee
Quang Vu <vuquangit@gmail.com>
2019-05-13 14:06:19 +0700

    Modify Nav

--- src/components/Navigation/Brand.js
+++ src/components/Navigation/Brand.js
@@ -5,16 +5,11 @@ import { Link } from "react-router-dom";
 export default class Brand extends React.Component {
   render() {
     return (
-      <div>
-        <img
-          className="myLogo"
-          src={myLogo}
-          alt="My Logo"
-          style={{ height: "40px", width: "auto" }}
-        />
-        <Link to={`/`} className="navbar-brand js-scroll-trigger">
+      <div className="navbar-brand">      
+        <Link to={`/`} > 
+          <img src={myLogo} width="30" height="30" alt="" className="d-inline-block align-top" />
           {this.props.value}
-        </Link>
+        </Link>     
       </div>
     );
   }
